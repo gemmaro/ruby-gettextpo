f487cd47bd5af0da5228eea5f133bfcc4bbe724f
gemmaro <gemmaro.dev@gmail.com>
2026-03-07 20:06:22 +0900

    Require grep.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,6 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((eval . (progn (add-to-list 'grep-find-ignored-directories "tmp")
+((nil . ((eval . (progn (require 'grep)
+                        (add-to-list 'grep-find-ignored-directories "tmp")
                         (add-to-list 'grep-find-ignored-directories "doc"))))))
