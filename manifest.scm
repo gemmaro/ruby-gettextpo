b81514063f805300dd5f5c482ed2e6ddf9f52f62
gemmaro <gemmaro.dev@gmail.com>
2026-03-15 11:48:11 +0900

    Switch to gettext version 1.0.

---
+++ manifest.scm
@@ -0,0 +1,43 @@
+;; Copyright (C) 2026  gemmaro
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+(use-modules (guix packages)
+             (guix download)
+             (guix profiles)
+             (gnu packages gettext)
+             (gnu packages gdb)
+             (gnu packages commencement)
+             (gnu packages llvm)
+             (gnu packages ruby)
+             (gnu packages ruby-check)
+             (gnu packages base))
+
+(define-public gnu-gettext-1.0
+  (package/inherit gnu-gettext
+    (version "1.0")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://gnu/gettext/gettext-" version
+                                  ".tar.gz"))
+              (sha256 (base32
+                       "06jgjxnm5xhw0d0zwl9birlwfxfgfqhk9q029j3h9941r5wrpnc5"))))))
+
+(packages->manifest (list gnu-gettext-1.0
+                          gcc-toolchain
+                          gdb
+                          clang
+                          ruby-3.4
+                          ruby-rake-compiler
+                          gnu-make))
